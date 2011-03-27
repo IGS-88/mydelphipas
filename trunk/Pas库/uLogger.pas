720f4a60c9743ad71e23d029d4088a6bcb59efe4
Codeup.WF@gmail.com <Codeup.WF@gmail.com@c5fb56a9-8158-9d95-4085-4ed1441a9c7e>
2011-03-27 13:09:03 +0000

    ! Start + Add Src + Add Pas库 + Add Release/LibAV + Add Dcu without *.dcu files

---
+++ trunk/Pas库/uLogger.pas
@@ -0,0 +1,132 @@
+unit uLogger;
+
+interface
+uses
+  SyncObjs, SysUtils, Classes, uGUID;
+type
+  TLogLevel = (llinfo, llerror, llexception, llwarn);
+  TLogInfo = record
+    ThreadID: Cardinal;
+    PntGUID: string;
+    LogLevel: TLogLevel;
+    LogMsg: String
+  end;
+  TOnLogEvent = procedure(Sender: TObject; const ALogInfo: TLogInfo) of object;
+  ELogger = class(Exception);
+
+  { 实际使用的接口 }
+  function WriteLog(AThreadID: Cardinal;const APntGUID:string; ALogLevel: TLogLevel; const ALogMsg: string): Boolean;
+  procedure SetOnLogEvent(OnLog: TOnLogEvent);
+  function GetOnLogEvent: TOnLogEvent;
+
+implementation
+type
+TLogger = class(TObject)
+  private
+    FLogInfo: TLogInfo;
+
+    FOnLog: TOnLogEvent;
+    procedure SetOnLog(value: TOnLogEvent);
+    function  ReadOnLog: TOnLogEvent;
+    class function createInstance: TLogger;
+  protected
+  public
+    ID: string;
+    constructor create;
+    destructor  destroy;override;
+    class function getInstance: TLogger;
+
+    function writeLog(AThreadID: Cardinal;const APntGUID:string; ALogLevel: TLogLevel; const ALogMsg: string): Boolean;
+    property  OnLog: TOnLogEvent read ReadOnLog write SetOnLog;
+  end;
+var
+  CS_create: TCriticalSection;
+  CS_write: TCriticalSection;
+
+{ 单元接口实现 }
+function WriteLog(AThreadID: Cardinal;const APntGUID:string; ALogLevel: TLogLevel; const ALogMsg: string): Boolean;
+begin
+  Result := TLogger.getInstance.writeLog(AThreadID, APntGUID, ALogLevel, ALogMsg);
+end;
+
+procedure SetOnLogEvent(OnLog: TOnLogEvent);
+begin
+  TLogger.getInstance.OnLog := OnLog;
+end;
+
+function GetOnLogEvent: TOnLogEvent;
+begin
+  Result := TLogger.getInstance.OnLog;
+end;
+
+{ TLogger }
+constructor TLogger.create;
+begin
+  raise ELogger.CreateFmt('不能手动创建单例类%s.',[classname]);
+end;
+
+class function TLogger.createInstance: TLogger;
+begin
+  Result := inherited NewInstance as TLogger;
+  Result.ID := GetGUID;
+  Result.FOnLog := nil;
+end;
+
+destructor TLogger.destroy;
+begin
+  { TODO : destroy }
+  inherited;
+end;
+
+class function TLogger.getInstance: TLogger;
+const
+  {$J+} Instance: TLogger = nil;{$J-}
+begin
+  CS_create.Acquire;
+  if Instance = nil then
+  begin
+    Instance := createInstance;
+  end;
+  Result := Instance;
+  CS_create.Leave;
+end;
+
+function TLogger.ReadOnLog: TOnLogEvent;
+begin
+  Result := TLogger.getInstance.FOnLog;
+end;
+
+procedure TLogger.SetOnLog(value: TOnLogEvent);
+begin
+  TLogger.getInstance.FOnLog := value;
+end;
+
+function TLogger.writeLog(AThreadID: Cardinal; const APntGUID: string;
+  ALogLevel: TLogLevel; const ALogMsg: string): Boolean;
+begin
+  with TLogger.getInstance do
+  begin
+    CS_write.Acquire;
+    with FLogInfo do
+    begin
+      ThreadID := AThreadID;
+      PntGUID := APntGUID;
+      LogLevel := ALogLevel;
+      LogMsg := ALogMsg;
+    end;
+    if Assigned(FOnLog) then
+    begin
+      FOnLog(Self, FLogInfo);
+    end;
+    CS_write.Leave;
+  end;
+  Result := True;
+end;
+
+initialization
+  CS_create := TCriticalSection.Create;
+  CS_write := TCriticalSection.Create;
+finalization
+  CS_create.Free;
+  CS_write.Free;
+end.
---
+++ trunk/Src/Unit1.pas
@@ -0,0 +1,82 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, uScreenCapture, FFBaseComponent, FFLog, StdCtrls, ExtCtrls, SingletonSubClassExamples;
+
+type
+  TForm1 = class(TForm)
+    FFLogger1: TFFLogger;
+    mmo1: TMemo;
+    img1: TImage;
+    btn1: TButton;
+    btn2: TButton;
+    btn3: TButton;
+    btn4: TButton;
+    btn5: TButton;
+    Edit1: TEdit;
+    lbl1: TLabel;
+    procedure FormCreate(Sender: TObject);
+    procedure btn1Click(Sender: TObject);
+    procedure btn3Click(Sender: TObject);
+    procedure btn5Click(Sender: TObject);
+  private
+    { Private declarations }
+    procedure OnError(Sender: TObject; const ErrorInfo: string);
+    procedure OnPreviewBitmap(Sender: TObject; const APreviewInfo: TPreviewInfo);
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+  ScreenCapture: TScreenCapture;
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  DoubleBuffered := True;
+  ScreenCapture := TScreenCapture.Create(Self);
+  ScreenCapture.OnError := OnError;
+  ScreenCapture.OnPreviewBitmap := OnPreviewBitmap;
+  mmo1.Lines.Add('Create ScreenCapture');
+end;
+
+procedure TForm1.btn1Click(Sender: TObject);
+begin
+  ScreenCapture.PreviewBitmap := True;
+  ScreenCapture.ProgressInterval := 100;
+  ScreenCapture.SetCaptureOptions(Edit1.Text);
+  ScreenCapture.UseDefaultOO;
+  if not ScreenCapture.Start('ScreenCapture.mp4') then
+  ShowMessage('Error');
+end;
+
+procedure TForm1.OnError(Sender: TObject; const ErrorInfo: string);
+begin
+  mmo1.Lines.Add(ErrorInfo);
+end;
+
+procedure TForm1.btn3Click(Sender: TObject);
+begin
+  ScreenCapture.Stop;
+end;
+
+procedure TForm1.OnPreviewBitmap(Sender: TObject;
+  const APreviewInfo: TPreviewInfo);
+begin
+  img1.Picture.Assign(APreviewInfo.Bitmap);
+  
+end;
+
+procedure TForm1.btn5Click(Sender: TObject);
+begin
+  TFooA.Instance.VirtualMethod;
+  TFooB(TFooB.Instance).VirtualMethod;
+end;
+
+end.
