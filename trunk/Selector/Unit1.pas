5047474dad456e76d7cb0578297d338492114b82
codeup.wf@gmail.com <codeup.wf@gmail.com@c5fb56a9-8158-9d95-4085-4ed1441a9c7e>
2011-05-12 10:44:37 +0000

    - 调试未通过

--- trunk/Selector/Unit1.pas
+++ trunk/Selector/Unit1.pas
@@ -14,8 +14,16 @@ type
     procedure btn1Click(Sender: TObject);
     procedure btn2Click(Sender: TObject);
     procedure btn3Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
+  protected
+    procedure WMMoving(var Message: TWMMoving); message WM_MOVING;
+    procedure WMNCHitTest(var Message: TWMNCHitTest); message WM_NCHITTEST;
+    procedure WMMove(var Message: TWMMove); message WM_MOVE;
+    procedure WMSize(var Message: TWMSize); message WM_SIZE;
+                                                           
     { Private declarations }
+
   public
     { Public declarations }
   end;
@@ -48,6 +56,32 @@ begin
   r.TopLeft := ScreenToClient(r.TopLeft);
   r.BottomRight := ScreenToClient(r.BottomRight);
   ShowMessage('l:'+IntToStr(r.Left)+' r:'+inttostr(r.Right)+' t:'+IntToStr(r.Top)+' b:'+IntToStr(r.Bottom));
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+//  SetWindowLong(application.handle,GWL_ExSTYLE,WS_EX_TOOLWINDOW);
+end;
+
+procedure TForm1.WMMove(var Message: TWMMove);
+begin
+  Caption := '1';
+end;
+
+procedure TForm1.WMMoving(var Message: TWMMoving);
+begin
+  Caption := IntToStr(Left);
+end;
+
+procedure TForm1.WMNCHitTest(var Message: TWMNCHitTest);
+begin
+//  Message.Result := HTCAPTION;
+  inherited;
+end;
+
+procedure TForm1.WMSize(var Message: TWMSize);
+begin
+  Caption := '555';
 end;
 
 end.
